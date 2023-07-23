060532388d5cacb5cb2789986215b74db68b0076
Michael Wadley <michael@mwdle.com>
2023-07-23 00:16:01 -0600

    Finished implementation for also restarting the tplink extender

--- src/test/java/Rebooter.java
+++ src/test/java/Rebooter.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Stopwatch;
+import org.apache.commons.lang3.time.StopWatch;
 import org.openqa.selenium.By;
 
 import java.io.*;
@@ -7,6 +9,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import static com.codeborne.selenide.Condition.interactable;
 import static com.codeborne.selenide.Selenide.$;
@@ -14,7 +17,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class Rebooter {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         Properties properties = new Properties();
         try (InputStream input = new FileInputStream("/home/mwdle/Desktop/JavaProjects/RouterRebooter/src/test/java/secrets.properties")) {
@@ -31,10 +34,17 @@ public class Rebooter {
 
         open("http://192.168.0.1/?util_restart");
 
+        StopWatch timer = new StopWatch();
+        timer.start();
+
         // Wait until the login screen appears to do anything
         while (!$(By.linkText("Login")).exists()) {
             Selenide.sleep(200);
+            if (timer.getTime(TimeUnit.SECONDS) > 60)
+                throw new Exception("Timed out loading router login page");
         }
+        timer.stop();
+        timer.reset();
 
         // Login to the restart utility
         $(By.id("UserName")).setValue(username);
@@ -49,19 +59,10 @@ public class Rebooter {
             $(By.className("ui-button")).click();
         }
 
-        // Click the button to restart the router
-        //$(By.className("submitBtn")).shouldBe(interactable).click();
-
-        //// Accept the javascript prompt alert
-        //Selenide.switchTo().alert().accept();
-
-        //// Sleep for 10s before exiting to ensure the request had time to go through
-        //Selenide.sleep(10000);
-
-        Selenide.closeWebDriver();
-
+        /**
+         * The following for loop finds the IP address of the gateway for the TP-Link extender before restarting the router, so that the extender can also be restarted afterwards.
+         */
         String tpLinkIP = "";
-
         for (int i = 2; i < 254; i++) {
             String address = "192.168.0." + i;
             try {
@@ -93,10 +94,48 @@ public class Rebooter {
             }
         }
 
+        // Click the button to restart the router
+        //$(By.className("submitBtn")).shouldBe(interactable).click();
+
+        //// Accept the javascript prompt alert
+        //Selenide.switchTo().alert().accept();
+
+        //// Sleep for 10s before exiting to ensure the request had time to go through
+        //Selenide.sleep(10000);
+
+        Selenide.closeWebDriver();
+
         if (!tpLinkIP.equals("")) {
             open("http://" + tpLinkIP + "/");
         }
 
+        timer.start();
+        while (!$(By.className("password-text")).exists()) {
+            Selenide.sleep(200);
+            if (timer.getTime(TimeUnit.SECONDS) > 30)
+                throw new Exception("Timed out loading router login page");
+        }
+        timer.stop();
+        timer.reset();
+
+        $(By.className("password-text")).shouldBe(interactable).setValue(properties.getProperty("tplinkPassword"));
+        $(By.id("login-btn")).shouldBe(interactable).click();
+
+        timer.start();
+        while (!$(By.id("map_router")).exists()) {
+            Selenide.sleep(200);
+            if (timer.getTime(TimeUnit.SECONDS) > 10)
+                throw new Exception("Timed out loading tplink management page");
+        }
+        timer.stop();
+        timer.reset();
+
+        $(By.id("top-control-reboot")).shouldBe(interactable).click();
+
+        $(By.className("msg-btn-container")).find(By.className("btn-msg-ok")).shouldBe(interactable).click();
+
+        // Sleep for 5 seconds before closing the webdriver and ending the script.
+        Selenide.sleep(5000);
         Selenide.closeWebDriver();
     }
 }
