28aaedb925bee7099ab379625986d52775c763da
Michael Wadley <michael@mwdle.com>
2024-08-02 16:30:50 -0600

    Moved router rebooter script creation into entrypoint instead of dockerfile.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,10 @@ if [ ! -f "/RouterRebooter/data/ExtenderRebooter.log" ]; then
     echo "Not run yet" > "/RouterRebooter/data/ExtenderRebooter.log"
 fi
 
+
+if [ ! -f "/RouterRebooter/data/executeRebooter.sh" ]; then
+    echo "#!/bin/bash\nexport ROUTER_PASSWORD=\$(cat /RouterRebooter/secrets/router_pass)\nexport EXTENDER_PASSWORD=\$(cat /RouterRebooter/secrets/extender_pass)\njava -jar /RouterRebooter/data/Rebooter.jar \$1" > "/RouterRebooter/data/executeRebooter.sh"
+    chmod +x "/RouterRebooter/data/executeRebooter.sh"
+fi
+
 /usr/sbin/sshd -D
