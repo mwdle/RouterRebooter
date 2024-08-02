33fe864830a0273c1717a4056f6f94b91ca8b282
Michael Wadley <michael@mwdle.com>
2024-08-02 16:48:37 -0600

    Fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,11 @@ if [ ! -f "/RouterRebooter/data/ExtenderRebooter.log" ]; then
     echo "Not run yet" > "/RouterRebooter/data/ExtenderRebooter.log"
 fi
 
-
 if [ ! -f "/RouterRebooter/data/executeRebooter.sh" ]; then
-    echo "#!/bin/bash\nexport ROUTER_PASSWORD=\$(cat /RouterRebooter/secrets/router_pass)\nexport EXTENDER_PASSWORD=\$(cat /RouterRebooter/secrets/extender_pass)\njava -jar /RouterRebooter/data/Rebooter.jar \$1" > "/RouterRebooter/data/executeRebooter.sh"
+    echo "#!/bin/bash
+export ROUTER_PASSWORD=\$(cat /RouterRebooter/secrets/router_pass)
+export EXTENDER_PASSWORD=\$(cat /RouterRebooter/secrets/extender_pass)
+java -jar /RouterRebooter/data/Rebooter.jar \$1" > "/RouterRebooter/data/executeRebooter.sh"
     chmod +x "/RouterRebooter/data/executeRebooter.sh"
 fi
 
